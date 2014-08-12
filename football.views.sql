0fc63ae13b9cd618fcab86c8e1ef17d08e5d0abf
Gerald Bauer <gerald.bauer@gmail.com>
2014-08-12 19:29:00 +0200

    Update football.views.sql

--- football.views.sql
+++ football.views.sql
@@ -14,6 +14,39 @@ CREATE VIEW teams_list AS
               INNER JOIN countries c ON c.id = t.country_id;
 
 
+-----------------------------------
+--
+--  todo: add some examples ??
+
+-- todo: fix: add (optional) group ??
+
+
+CREATE VIEW games_list AS
+SELECT e."key" AS event_key,
+  ---    e.name  AS event_name   -- fix: add to schema or use league.title + season.title ??
+       l."key" AS league_key,
+       l.title AS league_name,
+       s."key" AS season_key,
+       s.title AS season_name,
+       c."key" AS country_key,
+       c.name  AS country_name,
+       r.title AS round_name,
+  --   r.pos   AS round_num,
+       t1.title AS team1_name,
+       t1."key" AS team1_key,
+       t2.title AS team2_name,
+       t2."key" AS team2_key,
+       g.*
+  FROM games g
+   LEFT OUTER JOIN teams t1 ON t1.id = g.team1_id
+   LEFT OUTER JOIN teams t2 ON t2.id = g.team2_id
+   INNER JOIN rounds r ON r.id = g.round_id
+   INNER JOIN events e ON e.id = r.event_id
+   INNER JOIN leagues l ON l.id = e.league_id
+   INNER JOIN seasons s ON s.id = e.season_id
+   INNER JOIN countries c ON c.id = l.country_id;
+
+
 ----
 -- use like e.g. list German Bundesliga seasons
 --
