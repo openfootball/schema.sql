5564765dd05b16cb416c9377d72ce2ef69f88fff
Gerald Bauer <gerald.bauer@gmail.com>
2014-08-15 16:22:22 +0200

    up views

--- football.events.views.sql
+++ football.events.views.sql
@@ -22,26 +22,27 @@ CREATE VIEW events_teams_list AS
 --    FROM events_games_list
 --   WHERE event_key = 'world.2014'
 
-
 -- todo: fix: add (optional) group ??
 
--- todo: check why left outer join ??
 
 CREATE VIEW events_games_list AS
 SELECT e."key" AS event_key,
   ---    e.name  AS event_name   -- fix: add to schema or use league.title + season.title ??
        r.title AS round_name,
   --   r.pos   AS round_num,
+       groups.title AS group_name,
        t1.title AS team1_name,
        t1."key" AS team1_key,
        t2.title AS team2_name,
        t2."key" AS team2_key,
        g.*
   FROM games g
-   LEFT OUTER JOIN teams t1 ON t1.id = g.team1_id
-   LEFT OUTER JOIN teams t2 ON t2.id = g.team2_id
-   INNER JOIN rounds r ON r.id = g.round_id
-   INNER JOIN events e ON e.id = r.event_id;
+    INNER JOIN teams t1 ON t1.id = g.team1_id
+    INNER JOIN teams t2 ON t2.id = g.team2_id
+    INNER JOIN rounds r ON r.id = g.round_id
+    INNER JOIN events e ON e.id = r.event_id
+    LEFT OUTER JOIN groups ON groups.id = g.group_id     -- note: group for match table is optional
+    ;
 
 
 -------------------------
--- football.views.sql
+++ football.views.sql
@@ -18,8 +18,6 @@ CREATE VIEW teams_list AS
 --
 -- todo: add some examples ??
 
--- todo: fix: add (optional) group ??
-
 
 CREATE VIEW games_list AS
 SELECT e."key" AS event_key,
@@ -30,22 +28,25 @@ SELECT e."key" AS event_key,
        s.title AS season_name,
        c."key" AS country_key,
        c.name  AS country_name,
+       groups.title AS group_name,
+  --   groups.pos  AS group_num, ???
        r.title AS round_name,
-  --   r.pos   AS round_num,
+  --   r.pos   AS round_num, ???
        t1.title AS team1_name,
        t1."key" AS team1_key,
        t2.title AS team2_name,
        t2."key" AS team2_key,
        g.*
   FROM games g
-   LEFT OUTER JOIN teams t1 ON t1.id = g.team1_id
-   LEFT OUTER JOIN teams t2 ON t2.id = g.team2_id
-   INNER JOIN rounds r ON r.id = g.round_id
-   INNER JOIN events e ON e.id = r.event_id
-   INNER JOIN leagues l ON l.id = e.league_id
-   INNER JOIN seasons s ON s.id = e.season_id
-   INNER JOIN countries c ON c.id = l.country_id;
-
+    INNER JOIN teams t1 ON t1.id = g.team1_id
+    INNER JOIN teams t2 ON t2.id = g.team2_id
+    INNER JOIN rounds r ON r.id = g.round_id
+    INNER JOIN events e ON e.id = r.event_id
+    INNER JOIN leagues l ON l.id = e.league_id
+    INNER JOIN seasons s ON s.id = e.season_id
+    LEFT OUTER JOIN countries c ON c.id = l.country_id  -- note: countries for league table is optional (that is, might be null/empty)
+    LEFT OUTER JOIN groups ON groups.id = g.group_id     -- note: group for match table is optional
+    ;
 
 -------------------------------
 -- use like e.g. list German Bundesliga seasons
@@ -71,4 +72,6 @@ CREATE VIEW events_list AS
          FROM events e
               INNER JOIN seasons s ON s.id = e.season_id
               INNER JOIN leagues l ON l.id = e.league_id
-              INNER JOIN countries c ON c.id = l.country_id;
+              LEFT OUTER JOIN countries c ON c.id = l.country_id   -- note: countries for league table is optional (that is, might be null/empty)
+              ;
+
